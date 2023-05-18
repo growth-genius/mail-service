91ab0c89563f8013be18e6ff3d5f689cb4579591
yeji.jo <choyeji1591@gmail.com>
2023-05-18 21:21:12 +0900

    feat : 카프카 producer, consumer 설정 추가

---
+++ src/main/java/com/sgyj/mailservice/modules/service/kafka/MailKafkaConsumer.java
@@ -0,0 +1,56 @@
+package com.sgyj.mailservice.modules.service.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sgyj.mailservice.infra.mail.EmailMessage;
+import com.sgyj.mailservice.infra.mail.EmailService;
+import com.sgyj.mailservice.modules.dto.AccountDto;
+import com.sgyj.mailservice.modules.enums.MailSubject;
+import java.util.concurrent.CountDownLatch;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailKafkaConsumer {
+
+    private final EmailService emailService;
+    private final ObjectMapper objectMapper;
+    private String email;
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    @Transactional
+    @KafkaListener(topics = "password-mail-topic")
+    public void processSendPasswordByMail(String kafkaMessage) throws JsonProcessingException {
+        log.info("Kafka Message : ===> " + kafkaMessage);
+        AccountDto accountDto = objectMapper.readValue(kafkaMessage, AccountDto.class);
+        email = accountDto.getEmail();
+        latch.countDown();
+        emailService.sendEmail(createEmailByAuthCode(accountDto.getEmail(), MailSubject.VALID_AUTHENTICATION_ACCOUNT.getSubject()));
+
+    }
+
+    private EmailMessage createEmailByAuthCode(String email, String subject) {
+        String authCode = RandomStringUtils.randomAlphanumeric(12);
+        log.debug("AuthCode : " + authCode);
+        return createEmailMessage(email, subject, authCode);
+    }
+
+    private EmailMessage createEmailMessage(String email, String subject, String authCode) {
+        return EmailMessage.builder().to(email).subject(subject).message(authCode).build();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
