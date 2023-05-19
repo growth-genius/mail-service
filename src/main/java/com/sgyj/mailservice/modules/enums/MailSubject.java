aa747de077065e7a1177c625111cbd579930b470
yeji.jo <choyeji1591@gmail.com>
2023-05-19 17:03:20 +0900

    feat : MailSubject htmlCode 및 enum 추가

--- src/main/java/com/sgyj/mailservice/modules/enums/MailSubject.java
+++ src/main/java/com/sgyj/mailservice/modules/enums/MailSubject.java
@@ -5,11 +5,20 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum MailSubject {
-    VALID_AUTHENTICATION_ACCOUNT("TGather 회원가입 인증 메일");
+    VALID_AUTHENTICATION_ACCOUNT("TGather 회원가입 인증 메일", "validAuthenticationMail"), CONFIRM_JOIN_MEMBER("TravelGroup 가입 요청 메일", "confirmJoinMemberMail");
+
 
     private final String subject;
+
+    private final String htmlCode;
 
     public String getSubject() {
         return subject;
     }
+
+    public String getHtmlCode() {
+        return htmlCode;
+    }
+
+
 }
